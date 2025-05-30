bcc50936d2028f21b260d859b15db0c8d298f93b
Santiago Bernardino <santibberna@gmail.com>
2025-05-30 16:49:20 +0200

    [Fix] Static linking

--- tiledcpp/config.hpp
+++ tiledcpp/config.hpp
@@ -1,11 +1,17 @@
 #pragma once
 
-#ifdef _WIN32
-#ifdef TILEDCPP_EXPORT
+#if defined TILEDCPP_STATIC
+#define TILEDCPP_API
+#else
+
+#if defined _WIN32
+
+#if defined TILEDCPP_EXPORT
 #define TILEDCPP_API __declspec(dllexport)
 #else
 #define TILEDCPP_API __declspec(dllimport)
-#endif
-#else
-#define TILEDCPP_API
-#endif
+#endif // TILEDCPP_EXPORT
+
+#endif // _WIN32
+
+#endif // TILEDCPP_STATIC
